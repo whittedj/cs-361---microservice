6148df16da6316e588570d0b5d0afab3a81357fe
whittedj <55825175+whittedj@users.noreply.github.com>
2022-05-24 00:21:21 -0400

    Update time-translator.py

--- time-translator.py
+++ time-translator.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import os
 
 while True:
-    if os.stat("C:/Users/jonjw/PycharmProjects/cs-361 - Assignment 7/time.txt").st_size != 0:
-        f = open("C:/Users/jonjw/PycharmProjects/cs-361 - Assignment 7/time.txt", "r+")
+    if os.stat("time.txt").st_size != 0:
+        f = open("time.txt", "r+")
         start_array = f.readline().split()
         end_array = f.readline().split()
         
@@ -15,6 +15,6 @@ while True:
         seconds = difference.total_seconds()
         assigned_num = seconds/3600
 
-        t = open("C:/Users/jonjw/PycharmProjects/cs-361 - Assignment 7/translated-num.txt", "w")
+        t = open("translated-num.txt", "w")
         t.write(str(assigned_num))
         t.close()
